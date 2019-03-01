0ad1d6721ea7c9fd89c1e919cb7627812d08040e
bin-y <wye.mia.kid.c@gmail.com>
2019-03-01 23:26:29 +0800

    logging: add timestamp to log

--- src/logging.cpp
+++ src/logging.cpp
@@ -2,11 +2,25 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 
+#include <boost/log/expressions.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <iostream>
 namespace dnstoy {
+
+namespace expr = boost::log::expressions;
+
+const auto date_time_formatter =
+    expr::stream << expr::format_date_time<boost::posix_time::ptime>(
+                        "TimeStamp", "e[15m%m%d %H:%M:%S.%f")
+                 << expr::message;
 
 void InitLogging() {
   boost::log::add_common_attributes();
-  boost::log::add_console_log();
+  boost::log::add_console_log()->set_formatter(date_time_formatter);
+  ;
 }
 
 }  // namespace dnstoy
