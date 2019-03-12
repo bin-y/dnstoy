43a21d802b5ec7e5d0c456a5e33a26df51564639
bin-y <wye.mia.kid.c@gmail.com>
2019-03-13 00:38:07 +0800

    dns: define TransportTypeFlags

--- src/dns.hpp
+++ src/dns.hpp
@@ -5,4 +5,15 @@
 #include "dns_message_decoder.hpp"
 #include "dns_message_encoder.hpp"
 
+namespace dns {
+
+struct TransportTypeFlags {
+  static constexpr uintptr_t UDP = 1 << 0;
+  static constexpr uintptr_t TCP = 1 << 1;
+  static constexpr uintptr_t HTTPS = 1 << 2;
+  static constexpr uintptr_t TLS = 1 << 3;
+};
+
+}  // namespace dns
+
 #endif  // DNSTOY_DNS_H_
